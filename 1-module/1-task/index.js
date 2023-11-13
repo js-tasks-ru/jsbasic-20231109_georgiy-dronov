fb34238ab36b88f1bd00711136f07426665db6dd
Georg Dronov <dronovgeorg72@gmail.com>
2023-11-13 23:00:39 +0300

    создан факториал через цикл for

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,16 @@
 function factorial(n) {
   // ваш код...
+  let numberFactorial = n;
+  let result = n;
+  let count = --numberFactorial;
+
+  if (n === 1 || n === 0) {
+    return result = 1;
+  } else {
+    for (i = count; i > 1; i--) {
+      result *= i;
+      console.log(result);
+    }
+    return result;
+  }
 }
