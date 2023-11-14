e1e55b645963e01e680bbc77312fcb0a8500d783
Georg Dronov <dronovgeorg72@gmail.com>
2023-11-14 19:54:50 +0300

    Решена задача - Усечение строки

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,7 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  if (str.length > maxlength) {
+    let cutString = str.slice(0, (maxlength - 1)) + '…'
+    return cutString
+  }
+  return str
 }
