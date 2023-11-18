f6f2834b502de2a18e76d0e708874127a2f73ed7
Georg Dronov <dronovgeorg72@gmail.com>
2023-11-18 12:47:05 +0300

    Решена задача Функция sumSalary

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,13 @@
 function sumSalary(salaries) {
-  // ваш код...
+
+  let result = 0
+
+  for (let key in salaries) {
+    if (!isFinite(salaries[key])) {
+      continue
+    } else if (typeof salaries[key] === 'number') {
+      result += salaries[key]
+    }
+  }
+  return result
 }
