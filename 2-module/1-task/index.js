f617f5cd6df7073e089029a544c04e614cf0eb67
Georg Dronov <dronovgeorg72@gmail.com>
2023-11-18 13:59:54 +0300

    Правки для задачи Сумирования ЗП

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,9 +3,7 @@ function sumSalary(salaries) {
   let result = 0
 
   for (let key in salaries) {
-    if (!isFinite(salaries[key])) {
-      continue
-    } else if (typeof salaries[key] === 'number') {
+    if (isFinite(salaries[key]) && typeof salaries[key] === 'number') {
       result += salaries[key]
     }
   }
