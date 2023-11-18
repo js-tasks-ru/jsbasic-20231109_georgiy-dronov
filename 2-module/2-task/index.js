39fa8d5e985f8565779df27a5cc366996742a709
Georg Dronov <dronovgeorg72@gmail.com>
2023-11-18 14:17:19 +0300

    решена задача Проверка на пустой объект

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,16 @@
 function isEmpty(obj) {
-  // ваш код...
+
+  let keyCount = null;
+
+  for (let key in obj) {
+    keyCount += 1
+  }
+
+  if (keyCount !== null) {
+    return false
+  }
+  return true
 }
+
+
+
