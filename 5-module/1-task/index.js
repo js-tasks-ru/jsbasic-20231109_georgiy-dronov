3b9740a377bdc2737648cc36159dc93f9171507d
Georg Dronov <dronovgeorg72@gmail.com>
2023-12-02 10:21:43 +0300

    Решена вторая задачи и правки для первой

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,6 +1,6 @@
 function hideSelf() {
   const button = document.querySelector('.hide-self-button')
   button.addEventListener('click', function () {
-    return button.setAttribute('hidden', true)
+    return button.hidden = true
   })
 }
