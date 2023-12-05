9ffa1136c9e22538988295a005a7576da1449770
Georg Dronov <dronovgeorg72@gmail.com>
2023-12-05 15:08:42 +0300

    Готова задача 1

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,38 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows || this.rows
+    this.elem = this.createTable()
+  }
+
+  createTable() {
+    const table = document.createElement('table')
+    table.innerHTML = `
+      <thead>
+        <tr>
+          <th>Имя</th>
+          <th>Возраст</th>
+          <th>Зарплата</th>
+          <th>Город</th>
+          <th></th>
+        </tr>
+        </thead>
+        <tbody>
+            ${this.rows.map(row => this.createTableRow(row)).join('')}
+        </tbody>
+    `
+    return table
+  }
+
+  createTableRow(row) {
+    const tr = document.createElement('tr')
+    tr.innerHTML = `
+      <td>${row.name}</td>
+        <td>${row.age}</td>
+        <td>${row.salary}</td>
+        <td>${row.city}</td>
+        <td><button onclick="this.parentNode.parentNode.remove()">X</button></td>
+    `
+    return tr.outerHTML
   }
 }
